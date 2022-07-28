fb9f327c8fe034c0f1237eeb30ebc1727b464825
Juan Vivanco <juansebastianvc@gmail.com>
2022-07-27 23:31:42 -0500

    add more plans on pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -105,18 +105,19 @@ export function Pricing() {
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
             <span className="relative whitespace-nowrap">
               <SwirlyDoodle className="absolute top-1/2 left-0 h-[1em] w-full fill-pink-500/80" />
-              <span className="relative">Simple pricing,</span>
+              <span className="relative">Precios asequibles,</span>
             </span>{' '}
-            for everyone.
+            para todos.
           </h2>
           <p className="mt-4 text-lg text-slate-400">
-            It doesn’t matter what size your business is, our software won’t
-            work well for you.
+            No importa el tamaño de su negocio, nuestro software
+            funcionará bien para ti.
           </p>
         </div>
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
           <Plan
-            name="Starter"
+            featured
+            name="LITE"
             price="$9"
             description="Good for anyone who is self-employed and just getting started."
             href="/register"
@@ -129,8 +130,7 @@ export function Pricing() {
             ]}
           />
           <Plan
-            featured
-            name="Small business"
+            name="F20"
             price="$15"
             description="Perfect for small / medium sized businesses."
             href="/register"
@@ -145,7 +145,49 @@ export function Pricing() {
             ]}
           />
           <Plan
-            name="Enterprise"
+            name="F50"
+            price="$39"
+            description="For even the biggest enterprise companies."
+            href="/register"
+            features={[
+              'Send unlimited quotes and invoices',
+              'Connect up to 15 bank accounts',
+              'Track up to 200 expenses per month',
+              'Automated payroll support',
+              'Export up to 25 reports, including TPS',
+            ]}
+          />
+           <Plan
+            name="F100"
+            price="$9"
+            description="Good for anyone who is self-employed and just getting started."
+            href="/register"
+            features={[
+              'Send 10 quotes and invoices',
+              'Connect up to 2 bank accounts',
+              'Track up to 15 expenses per month',
+              'Manual payroll support',
+              'Export up to 3 reports',
+            ]}
+          />
+          <Plan
+            featured
+            name="F-ilimitado"
+            price="$15"
+            description="Perfect for small / medium sized businesses."
+            href="/register"
+            features={[
+              'Send 25 quotes and invoices',
+              'Connect up to 5 bank accounts',
+              'Track up to 50 expenses per month',
+              'Automated payroll support',
+              'Export up to 12 reports',
+              'Bulk reconcile transactions',
+              'Track in multiple currencies',
+            ]}
+          />
+          <Plan
+            name="Contable"
             price="$39"
             description="For even the biggest enterprise companies."
             href="/register"
