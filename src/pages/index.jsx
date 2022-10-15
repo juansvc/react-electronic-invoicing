2eba468e10f73e61502aec1787991f7b5afcb511
Juan Vivanco <juansebastianvc@gmail.com>
2022-10-14 23:49:16 -0500

    solve hydratation next.js caused by new carrousel and logos

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,24 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 
 import { CallToAction } from '@/components/CallToAction'
 import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { Hero } from '@/components/Hero'
 import { Pricing } from '@/components/Pricing'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
 import { Testimonials } from '@/components/Testimonials'
 
 export default function Home() {
+  const Hero = dynamic(() => import('@/components/Hero'), { ssr: false })
   return (
     <>
       <Head>
         <title>Tfactura - Sistema de facturación electrónica</title>
         <meta
-          name="description"
-          content="Tfactura - Sistema de facturación electrónica."
+          name='description'
+          content='Tfactura - Sistema de facturación electrónica.'
         />
       </Head>
       <Header />
